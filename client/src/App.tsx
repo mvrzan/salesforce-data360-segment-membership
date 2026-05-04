b7e991fa1cc57c1e851b0edbe3ebbdf58c395198
mvrzan <mvrzan@icloud.com>
2026-05-04 11:40:17 -0400

    add IndividualDetailPage route to app navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,17 @@ import { Routes, Route, Navigate } from "react-router";
 import Layout from "./layout/Layout";
 import SegmentsPage from "./pages/SegmentsPage";
 import IndividualsPage from "./pages/IndividualsPage";
+import IndividualDetailPage from "./pages/IndividualDetailPage";
 
 const App = () => (
   <Layout>
     <Routes>
       <Route path="/" element={<SegmentsPage />} />
       <Route path="/segments/:segmentApiName" element={<IndividualsPage />} />
+      <Route
+        path="/segments/:segmentApiName/individuals/:individualId"
+        element={<IndividualDetailPage />}
+      />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </Layout>
